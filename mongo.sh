7648cdc80948a677f3aa484de20372ee4bce5c94
team-poi <korearider155@gmail.com>
2025-04-26 09:18:25 +0900

    fix script

--- mongo.sh
+++ mongo.sh
@@ -1,10 +1,7 @@
 while true; do
     NOW=`date '+%F_%H:%M:%S'`;
     mongodump --gzip --archive="$NOW.archive" --uri="$MONGO_URI"
-    curl \
-        -F 'payload_json={"content": ""}' \
-        -F "file1=@$NOW.archive" \
-        $WEBHOOK_URL
+    curl -F 'payload_json={"content": "Backup complete"}' -F "file1=@$NOW.archive" $WEBHOOK_URL
     rm "$NOW.archive"
     echo "Backup and upload completed at $NOW"
     sleep 86400
