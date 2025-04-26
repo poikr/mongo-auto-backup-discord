b56bfba62ee1897ec20e62cf8a116f6e8f315d16
team-poi <korearider155@gmail.com>
2025-04-26 09:21:18 +0900

    add more logs to script

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,10 @@
 while true; do
     NOW=`date '+%F_%H:%M:%S'`;
+    echo "Starting backup at $NOW"
     mongodump --gzip --archive="$NOW.archive" --uri="$MONGO_URI"
-    curl -F 'payload_json={"content": "Backup complete"}' -F "file1=@$NOW.archive" $WEBHOOK_URL
+    echo "Dump completed at $NOW"
+    curl -F 'payload_json={"content": "Backup complete"}' -F "file1=@$NOW.archive" "$WEBHOOK_URL"
+    echo "File uploaded to Discord at $NOW"
     rm "$NOW.archive"
     echo "Backup and upload completed at $NOW"
     sleep 86400
